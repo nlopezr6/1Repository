88a915d14bbbcec687ebd4020ddf4dbb98a9071b
Duvan lopez <nlopezr6@ucentral.edu.co>
2024-04-23 00:28:46 -0500

    proyecto base

---
+++ src/main/java/com/ucentral/regventa/registrarv/RegistrarvApplication.java
@@ -0,0 +1,13 @@
+package com.ucentral.regventa.registrarv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrarvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrarvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ucentral/regventa/registrarv/RegistrarvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucentral.regventa.registrarv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrarvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
